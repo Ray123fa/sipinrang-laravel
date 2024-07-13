82c02d5a60c62e194c87cf43187ffb520d0d604f
Rayhan F <rayhanfrdh123@gmail.com>
2024-07-13 21:56:33 +0800

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,18 +16,21 @@ class DatabaseSeeder extends Seeder
         $superadmin = [
             'name' => 'Superadmin',
             'email' => 'superadmin@mail.com',
+            'email_verified_at' => now(),
             'password' => bcrypt('superadmin123'),
             'role' => 'superadmin'
         ];
         $admin = [
             'name' => 'Admin',
             'email' => 'admin@mail.com',
+            'email_verified_at' => now(),
             'password' => bcrypt('admin123'),
             'role' => 'admin'
         ];
         $user = [
             'name' => 'User',
             'email' => 'user@mail.com',
+            'email_verified_at' => now(),
             'password' => bcrypt('user123'),
             'role' => 'user'
         ];
